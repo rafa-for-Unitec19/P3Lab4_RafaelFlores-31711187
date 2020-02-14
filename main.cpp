eaa8824c63b4e0c475a9e163645947e70f993cd6
Rafael <rafac@unitec.edu>
2020-02-14 17:46:40 -0600

    Acciones de Usuario

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@ bool isAdmin = false;
 void imprimir(int ind){
 	cout << "A continuacion se listan los libros existentes" << endl;
 	for (int i = 0; i < ind; i++){
-		if (arrL[i].getTitulo() != ""){
-			cout << (i+1) << " -> " << arrL[i].getTitulo() << " Precio -> " << arrL[i].getPrecio() << endl;
+		if (arrL[i].getTitulo() != "" && arrL[i].getEstado()){
+			cout << (i+1) << " -> " << arrL[i].getTitulo() << " | Precio -> " << arrL[i].getPrecio() << endl;
 		}
 	}
 }
@@ -26,7 +26,8 @@ int menu(){
 		<< "\n\t3. Buscar por Titulo"
 		<< "\n\t4. Agregar dinero a mi cuenta"
 		<< "\n\t5. Restablecer contraseña"
-		<< "\n\t6. Borrar Cuenta"<< endl;
+		<< "\n\t6. Borrar Cuenta"
+		<< "\n\t7. Salir"<< endl;
 	cin >> op;
 	return op;
 }
@@ -37,7 +38,8 @@ int menuAdmin(){
 		<< "\n\t1. Agregar Libro"
 		<< "\n\t2. Modificar Libro"
 		<< "\n\t3. Eliminar Libro"
-		<< "\n\t4. Borrar registro de libros" << endl;
+		<< "\n\t4. Borrar registro de libros"
+	        << "\n\t5. Salir"<< endl;
 	cin >> op;
 	return op;
 }
@@ -65,11 +67,13 @@ Usuario Login(){
 
 Usuario menuLogin(bool &flag){
 	Usuario act;
-	while(true){
+	bool salida = true;
+	while(salida){
 		int op;
 		cout << "\nSeleccione la opcion que desea:"
 			<< "\n\t1. Ingresar"
-			<< "\n\t2. Registrarse" << endl;
+			<< "\n\t2. Registrarse" 
+			<< "\n\t3. Salir"<< endl;
 		cin >> op;
 		switch(op){
 			case 1:
@@ -85,10 +89,14 @@ Usuario menuLogin(bool &flag){
 			case 2:
 				cout << "Bienvenido al Registro" << endl;
 				break;
+			case 3:
+				salida = false;
+				break;
 			default:
 				cout << "La opcion es incorrecta" << endl;
 		}
 	}
+	return act;
 }
 
 void Agregar(int &ind){
@@ -98,8 +106,7 @@ void Agregar(int &ind){
 	string t; getline(cin, t);
 	l.setTitulo(t);
 	cout << "Ingrese el nombre del autor" << endl;
-	string a; getline(cin, a);
-	cin.ignore();
+	string a; cin >> a;
 	l.setAutor(a);
 	cin.get();
 	while(true){
@@ -120,6 +127,7 @@ void Eliminar(int ind){
 		cin >> op;
 		if (op > 0 && op < ind){
 			arrL[op].setTitulo("");
+			break;
 		}else{
 			cout << "No existe ese libro!!!!" << endl;
 		}
@@ -146,21 +154,26 @@ void selector(){
 	int tamLibro = 0;
 	int tamUsr = 2, index = tamLibro;
 	Usuario actual;
+
 		while(!salida){
 			actual = menuLogin(successLog);
+			if (actual.getNombre() == ""){
+				salida = true;
+			}
 		while(successLog){
 			if (isAdmin){
 				switch(menuAdmin()){
 					case 1: 
 						cout << "Agregar Libro" << endl;
 						if (tamLibro < 10){
-							index = fillSpace();
+							index = fillSpace(tamLibro);
+							cout << "El Valor de Index es: "<< index << endl;
 							if(index == tamLibro){
 								Agregar(tamLibro);
 							}else{
 								Agregar(index);
-								imprimir(tamLibro);
 							}
+							imprimir(tamLibro);
 						}else{
 							cout << "Ya no puede crear mas libros" << endl;
 						}
@@ -186,15 +199,77 @@ void selector(){
 						if (tamLibro == 0){
 							cout << "El arreglo esta vacio!!!" << endl;
 						}else{
-							tamLibro = 0;
 							blank(tamLibro);
+							tamLibro = 0;
 						}
+						break;
+					case 5: 
+						cout << "Saliendo de la cuenta" << endl;
+						successLog = false;
 						break;
 					default:
 							cout << "La opcion de admnistrador es incorrecta!!!" << endl;
 				}
 			}else{
 				switch(menu()){
+					case 1:{
+							cout << "Compra un libro: " << endl;
+							if (tamLibro != 0){
+								imprimir(tamLibro);
+								cout << "Que libro quiere comprar?" << endl;
+								int comp; cin >> comp;
+								if (comp > 0 && comp < tamLibro){
+									if (actual.getDinero() < arrL[comp].getPrecio()){
+										actual.setDinero(actual.getDinero() - arrL[comp].getPrecio());
+										arrL[comp].setEstado(false);
+										cout << "Ha comprado este libro!!!" << endl; 
+										cout << "El saldo para " << actual.getNombre() << " es: " << actual.getDinero() << endl;
+									}
+								}
+							}else{
+								cout << "No hay libros para vender" << endl;
+							}
+						break;
+					       }
+					case 2:
+					       cout << "Buscar por Titulo, Proximo Release" << endl;
+					       break;
+					case 3:
+					       cout << "Buscar por Autor, Proximo Release" << endl;
+					       break;
+					case 4:{
+							cout << "Agregar mas dinero a la cuenta" << endl;
+							cout << "Ingrese cuanto desee añadir a su cuenta" << endl;
+							int cant; cin >> cant;
+							if(cant < 1){
+								cout << "Tiene que agregar una cantidad positiva a la cuenta" << endl;
+							}else{
+								cout << "Actualmente tenia: " << actual.getDinero() << " dolares en su cuenta" << endl;
+								actual.setDinero(cant + actual.getDinero());
+							}	cout << "Ahora su cuenta tiene" << actual.getDinero() << "dolares en su cuenta" << endl;
+						break;
+					       }
+					case 5:{
+					       		cout << "Cambiar contraseña"
+								<< "Ingrese su vieja contraseña: " << endl;
+							string pss; cin >> pss;
+							if (pss == actual.getPass()){
+								cout << "Ingrese su nueva contraseña" << endl;
+								cin >> pss;
+								actual.setPass(pss);
+								cout << "Se ha actualizado la contraseña" << endl;
+							}
+							break;
+						}
+					case 6:
+						cout << "Borrando su cuenta..." << endl;
+						actual.setUsr("");
+						cout << "Volviendo al Login" << endl;
+						successLog = false;
+						break;
+					case 7: 
+						cout << "Saliendo de la cuenta..." << endl;
+						successLog = false;
 					default:
 						cout << "La opcion es incorrecta" << endl;
 				}
